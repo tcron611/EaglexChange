c17544b806651b39c17a9e7438902be8489a042f
Thomas Ronan <ronantho@bc.edu>
2019-04-20 14:47:50 -0400

    Added UI elements for TicketMaster search function

--- Eagle Xchange/ViewController.swift
+++ Eagle Xchange/ViewController.swift
@@ -10,11 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var categoryPickerView: UIPickerView!
+    
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var regionTextField: UITextField!
+    @IBOutlet weak var regionPickerView: UIPickerView!
+    
+    @IBOutlet weak var keyWordLabel: UILabel!
+    @IBOutlet weak var keyWordTextField: UITextField!
+    
+    @IBOutlet weak var sportLabel: UILabel!
+    @IBOutlet weak var basketballImageView: UIImageView!
+    @IBOutlet weak var footballImageView: UIImageView!
+    @IBOutlet weak var baseballImageView: UIImageView!
+    @IBOutlet weak var hockeyImageView: UIImageView!
+    
+    var category = ""
+    var market = ""
+    var keyWordSearch = ""
+    var sport = ""
+    
+    //Market Constants
+    let greaterBostonArea = "11"
+    
+    
+    let apiKey = "&apikey=WKsBVheH2lggazqKJrTtQvZKAiRZiCBS"
+    let apiString = "https://app.ticketmaster.com/discovery/v2/events.json?"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-
+    
+    @IBAction func basketBallPressed(_ sender: UITapGestureRecognizer) {
+        sport = "&classificationName=NBA"
+        print(sport)
+    }
+    
+    @IBAction func footBallPressed(_ sender: UITapGestureRecognizer) {
+        sport = "&classificationName=NFL"
+        print(sport)
+    }
+    
+    @IBAction func baseBallPressed(_ sender: UITapGestureRecognizer) {
+        sport = "&classificationName=MLB"
+        print(sport)
+    }
+    
+    @IBAction func hockeyPressed(_ sender: UITapGestureRecognizer) {
+        sport = "&classificationName=NFL"
+    }
+    
+    
 }
+/*
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.pokeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row+1). \(pokemon.pokeArray[indexPath.row].name)"
+        
+        // should I get more data?
+        print("indexPath.row = \(indexPath.row) pokeArray.count-1 = \(pokemon.pokeArray.count-1)")
+        if indexPath.row == pokemon.pokeArray.count-1 && pokemon.apiURL.hasPrefix("http") {
+            print("👊👊 Making a call to get more data! 👊👊")
+            activityIndicator.startAnimating()
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            pokemon.getPokemon {
+                self.tableView.reloadData()
+                self.navigationItem.title = "\(self.pokemon.pokeArray.count) of \(self.pokemon.totalPokemon) Pokemon"
+                self.activityIndicator.stopAnimating()
+                UIApplication.shared.endIgnoringInteractionEvents()
+            }
+        }
+        
+        return cell
+    }
+    
+    
+}
+
+*/
 
